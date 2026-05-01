abe350f3d144b3b6102e57c60253222fe00a1886
Aditya Bauthiyal <adityabauthiyal2007@gmail.com>
2026-05-02 00:44:29 +0530

    Delete src directory

--- src/data/doctors.js
+++
@@ -1,7 +0,0 @@
-const doctors = [
-  { id: 1, name: "Dr. Sharma", specialization: "Cardiologist" },
-  { id: 2, name: "Dr. Mehta", specialization: "Dentist" },
-  { id: 3, name: "Dr. Verma", specialization: "Dermatologist" }
-];
-
-export default doctors;
